54b1e05eb0142457c1752f70e25ea99a6dc4748e
diken666 <39002272+diken666@users.noreply.github.com>
2019-11-08 08:59:11 +0800

    Update app.js

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    // 跨域请求允许携带cookie
+    // res.setHeader("Access-Control-Allow-Credentials", "true");
     next();
 });
 
