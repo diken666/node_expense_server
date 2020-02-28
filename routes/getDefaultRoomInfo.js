5b508efa4fceac9f7812acadf314d3b68ed95432
diken666 <diken666@163.com>
2020-02-28 14:55:04 +0800

    update

--- routes/getDefaultRoomInfo.js
+++ routes/getDefaultRoomInfo.js
@@ -4,11 +4,18 @@ const db = require('../db/dbConnect');
 const sql = require('../dbSql/sql');
 const jwt = require('jsonwebtoken');
 
+// 获取默认房间信息
+// 房间号 上期水表数 本期水表数	本期电表数	本期电表数	本期用水(吨) 本期用电(度) 本期水费(元) 本期电费(元)	费用总计(元)
 router.get('/', (req, res) => {
     let uname = req.cookies["uname"];
     let token = req.cookies["token"];
+    console.log("--->", jwt.verify(token, uname));
     try {
-
+        jwt.verify(token, uname);
+        res.json({
+            state: "ok",
+            msg: "操作成功！"
+        });
     } catch( e ) {
         res.json({
             state: "error",
